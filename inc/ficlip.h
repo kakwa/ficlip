ad77e3a44a3046e1b605d510b2e9ba47660f31d5
kakwa <carpentier.pf@gmail.com>
2017-06-14 09:30:03 +0200

    adding various utility functions

--- inc/ficlip.h
+++ inc/ficlip.h
@@ -1,13 +1,23 @@
+/* This file is part of the ficlip clipping library
+ *
+ * ficlip is licensed under MIT.
+ *
+ * Copyright 2017, Pierre-Francois Carpentier
+ */
+
+/* Type of segments
+ */
 typedef enum {
-   FI_SEG_END = 0x00,
-   FI_SEG_MOVE = 0x01,
-   FI_SEG_LINE = 0x02,
-   FI_SEG_ARC = 0x03,
-   FI_SEG_BEZIER = 0x04,
+    FI_SEG_END = 0x00,
+    FI_SEG_MOVE = 0x01,
+    FI_SEG_LINE = 0x02,
+    FI_SEG_ARC = 0x03,
+    FI_SEG_BEZIER = 0x04,
 } FI_SEG_TYPE;
 
-typedef enum
-{
+/* List of operations possible
+ */
+typedef enum {
     FI_AND = 0x01,
     FI_OR = 0x02,
     FI_XOR = 0x03,
@@ -30,6 +40,29 @@ typedef struct _FI_PATH {
     struct _FI_PATH *next;
 } FI_PATH;
 
-int ficlip(FI_PATH p1, FI_PATH p2, FI_OPS operation);
+/* Build the clipping path from pathes "p1" and "p2" with opetration "ops"
+ * Result in FIPATH **out, return integer error code
+ */
+int ficlip(FI_PATH *p1, FI_PATH *p2, FI_OPS ops, FI_PATH **out);
+
+/* Add a new segment of a given type to a FI_PATH
+ */
+void fi_add_new_seg(FI_PATH **path, FI_SEG_TYPE type);
+
+/* Free a FI_PATH structure
+ */
+void fi_free_path(FI_PATH **path);
+
+/* Draw a FI_PATH in an SVG manner (M x1,y1 L x2,y2 C x3,y3 x4,y4 x5,y5 A x6,y6 x7,y7 Z)
+ */
+void fi_draw_path(FI_PATH *in, FILE *out);
+
+/* Copy a FI_PATH
+ */
+void fi_copy_path(FI_PATH *in, FI_PATH **out);
+
+/* Offset a FI_PATH
+ */
+void fi_offset_path(FI_PATH *in, FI_POINT_D pt);
 
 /* vim:set shiftwidth=2 softtabstop=2 expandtab: */
--- src/ficlip.c
+++ src/ficlip.c
@@ -1,3 +1,159 @@
+/* This file is part of the ficlip clipping library
+ *
+ * ficlip is licensed under MIT.
+ *
+ * Copyright 2017, Pierre-Francois Carpentier
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
 #include "ficlip.h"
 
-int ficlip(FI_PATH p1, FI_PATH p2, FI_OPS operation);
+void fi_point_draw_d(FI_POINT_D pt, FILE *out) {
+    fprintf(out, "%.4f,%.4f ", pt.x, pt.y);
+}
+
+int ficlip(FI_PATH *p1, FI_PATH *p2, FI_OPS ops, FI_PATH **out) {
+    return 0;
+}
+
+void fi_free_path(FI_PATH **path) {
+    if ((*path) == NULL) {
+        return;
+    }
+    FI_PATH *tmp1 = (*path);
+    FI_PATH *tmp2 = (*path);
+    while (tmp1 != NULL) {
+        tmp1 = tmp1->next;
+        free(tmp2->section.points);
+        free(tmp2);
+        tmp2 = tmp1;
+    }
+    (*path) = NULL;
+}
+
+void fi_draw_path(FI_PATH *in, FILE *out) {
+    FI_PATH *tmp = in;
+    while (tmp != NULL) {
+        FI_SEG_TYPE type = tmp->section.type;
+        FI_POINT_D *pt = tmp->section.points;
+        switch (type) {
+        case FI_SEG_END:
+            fprintf(out, "Z ");
+            break;
+        case FI_SEG_MOVE:
+            fprintf(out, "M ");
+            fi_point_draw_d(pt[0], out);
+            break;
+        case FI_SEG_LINE:
+            fprintf(out, "L ");
+            fi_point_draw_d(pt[0], out);
+            break;
+        case FI_SEG_ARC:
+            fprintf(out, "A ");
+            fi_point_draw_d(pt[0], out);
+            fi_point_draw_d(pt[1], out);
+            break;
+        case FI_SEG_BEZIER:
+            fprintf(out, "C ");
+            fi_point_draw_d(pt[0], out);
+            fi_point_draw_d(pt[1], out);
+            fi_point_draw_d(pt[2], out);
+            break;
+        }
+        tmp = tmp->next;
+    }
+}
+
+void fi_copy_path(FI_PATH *in, FI_PATH **out) {
+    FI_PATH *tmp = in;
+    FI_PATH *out_current = NULL;
+    while (tmp != NULL) {
+        FI_SEG_TYPE type = tmp->section.type;
+        FI_POINT_D *pt = tmp->section.points;
+        fi_add_new_seg(&out_current, type);
+        switch (type) {
+        case FI_SEG_END:
+            break;
+        case FI_SEG_MOVE:
+            out_current->last->section.points[0] = pt[0];
+            break;
+        case FI_SEG_LINE:
+            out_current->last->section.points[0] = pt[0];
+            break;
+        case FI_SEG_ARC:
+            out_current->last->section.points[0] = pt[0];
+            out_current->last->section.points[1] = pt[1];
+            break;
+        case FI_SEG_BEZIER:
+            out_current->last->section.points[0] = pt[0];
+            out_current->last->section.points[1] = pt[1];
+            out_current->last->section.points[2] = pt[2];
+            break;
+        }
+        tmp = tmp->next;
+    }
+    (*out) = out_current;
+}
+
+void fi_offset_path(FI_PATH *in, FI_POINT_D pt) {
+    FI_PATH *tmp = in;
+    while (tmp != NULL) {
+        FI_SEG_TYPE type = tmp->section.type;
+        switch (type) {
+        case FI_SEG_END:
+            break;
+        case FI_SEG_MOVE:
+            tmp->section.points[0].x += pt.x;
+            tmp->section.points[0].y += pt.y;
+            break;
+        case FI_SEG_LINE:
+            tmp->section.points[0].x += pt.x;
+            tmp->section.points[0].y += pt.y;
+            break;
+        case FI_SEG_ARC:
+            tmp->section.points[1].x += pt.x;
+            tmp->section.points[1].y += pt.y;
+            break;
+        case FI_SEG_BEZIER:
+            tmp->section.points[2].x += pt.x;
+            tmp->section.points[2].y += pt.y;
+            break;
+        }
+        tmp = tmp->next;
+    }
+}
+
+void fi_add_new_seg(FI_PATH **path, FI_SEG_TYPE type) {
+    FI_PATH *new_path = calloc(1, sizeof(FI_PATH));
+    FI_POINT_D *new_seg;
+    switch (type) {
+    case FI_SEG_END:
+        new_seg = NULL;
+        break;
+    case FI_SEG_MOVE:
+        new_seg = calloc(1, sizeof(FI_POINT_D));
+        break;
+    case FI_SEG_LINE:
+        new_seg = calloc(1, sizeof(FI_POINT_D));
+        break;
+    case FI_SEG_ARC:
+        new_seg = calloc(2, sizeof(FI_POINT_D));
+        break;
+    case FI_SEG_BEZIER:
+        new_seg = calloc(3, sizeof(FI_POINT_D));
+        break;
+    default:
+        new_seg = NULL;
+        break;
+    }
+    new_path->section.points = new_seg;
+    new_path->section.type = type;
+    if (*path == NULL || (*path)->last == NULL) {
+        *path = new_path;
+        new_path->last = new_path;
+    } else {
+        (*path)->last->next = new_path;
+        (*path)->last = new_path;
+    }
+}
