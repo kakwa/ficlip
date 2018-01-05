a57bc8ce95eff04cbe360d8b12ab87f39c2b19e0
kakwa <carpentier.pf@gmail.com>
2018-01-05 21:59:33 +0100

    fix some error in the valgrind check

--- tests/valgrind_run.sh
+++ tests/valgrind_run.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+ret=0
 VALGRIND_CMD="valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes --error-exitcode=42"
 
-for f in *-test*
+for f in *test*
 do
-$VALGRIND_CMD ./$f
+if [ -f $f ] && [ -x $f ]
+then
+    $VALGRIND_CMD ./$f
+    ret=$(( $? + $ret ))
+fi
 done
+
+exit $ret
