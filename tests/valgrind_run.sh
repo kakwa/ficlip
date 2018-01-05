ff4b8390d0f68ab00d259b218aa4a25199bcf89b
kakwa <carpentier.pf@gmail.com>
2018-01-05 21:56:09 +0100

    fix valgrind check

--- tests/valgrind_run.sh
+++ tests/valgrind_run.sh
@@ -2,4 +2,7 @@
 
 VALGRIND_CMD="valgrind --tool=memcheck --leak-check=yes --show-reachable=yes --num-callers=20 --track-fds=yes --error-exitcode=42"
 
-$VALGRIND_CMD $1
+for f in *-test*
+do
+$VALGRIND_CMD ./$f
+done
